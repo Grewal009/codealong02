87f7ba19ab0820d77c4238788e9a2f578f1ca5c8
Aneel Grewal <aneel.grewal@hotmail.com>
2024-05-30 22:35:06 +0200

    initial working

---
+++ ConsoleApp17/Tasks.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApp17;
+
+public class Tasks
+{
+    public string Oppgave1 { get; }
+
+    public string Day1 { get; }
+
+
+    public Tasks(string oppgave, string day)
+    {
+        Oppgave1 = oppgave;
+        Day1 = day;
+    }
+
+}
